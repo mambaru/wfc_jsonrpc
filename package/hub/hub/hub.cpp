2eb393e650f3f60e59f702cd2af8cd5de4161ac6
Vladimir Migashko <migashko@gmail.com>
2016-05-29 22:22:07 +0300

    upd

--- package/hub/hub/hub.cpp
+++ package/hub/hub/hub.cpp
@@ -12,12 +12,12 @@ void hub::reconfigure()
 }
 
 
-void hub::perform_incoming(incoming_holder holder, io_id_t io_id, rpc_outgoing_handler_t handler) 
+void hub::perform_incoming(incoming_holder /*holder*/, io_id_t /*io_id*/, rpc_outgoing_handler_t /*handler*/) 
 {
   if ( this->suspended()  ) return;
 }
   
-void hub::perform_outgoing(outgoing_holder holder, io_id_t io_id)
+void hub::perform_outgoing(outgoing_holder /*holder*/, io_id_t /*io_id*/)
 {
   if ( this->suspended() ) return;
 }
--- package/hub/hub/hub.hpp
+++ package/hub/hub/hub.hpp
@@ -18,6 +18,13 @@ public:
 // domain
   virtual void reconfigure() override;
 
+  /*
+  virtual void reg_io(io_id_t , std::weak_ptr<iinterface> );
+
+  virtual void unreg_io(io_id_t );
+
+  virtual void perform_io(data_ptr , io_id_t , outgoing_handler_t handler);
+  */
 // ijsonrpc
   virtual void perform_incoming(incoming_holder, io_id_t, rpc_outgoing_handler_t handler) override;
   virtual void perform_outgoing(outgoing_holder, io_id_t) override;
