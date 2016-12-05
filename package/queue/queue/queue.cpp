357452ed38e44099a1b67283758ab869fdb559d1
Vladimir Migashko <migashko@gmail.com>
2016-12-06 01:32:58 +0300

    code clean

--- package/queue/queue/queue.cpp
+++ package/queue/queue/queue.cpp
@@ -8,51 +8,11 @@ queue::queue()
 
 void queue::ready()
 {
- // domain_proxy::reconfigure();
   const auto& opt = this->options();
-  /*
-  if ( !opt.incoming_disabled )
-  {
-    if ( opt.incoming_workflow.empty() )
-    {
-      _incoming = this->get_workflow();
-    }
-    else
-    {
-      _incoming = this->get_target< ::wfc::workflow >( opt.incoming_workflow );
-    }
-  }
-  else
-  {
-    _incoming = nullptr;
-  }
-  */
-  
   if ( opt.callback_queue == true )
     _callback_workflow = this->get_workflow( opt.callback_workflow );
   else
     _callback_workflow = nullptr;
-  
-  /*
-  if ( !opt.outgoing_disabled )
-  {
-    if ( opt.outgoing_workflow.empty() )
-    {
-      _outgoing = this->get_workflow();
-    }
-    else
-    {
-      _outgoing = this->get_target< ::wfc::workflow >( opt.outgoing_workflow );
-    }
-
-    //_outgoing = ::wfc::workflow::recreate_and_start( _outgoing, opt.outgoing );
-  }
-  else 
-  {
-    //if ( _outgoing!=nullptr ) _outgoing->stop();
-    _outgoing = nullptr;
-  }
-  */
 }
 
 void queue::stop(const std::string&)
@@ -65,7 +25,6 @@ void queue::perform_incoming(incoming_holder holder, io_id_t io_id, rpc_outgoing
   if ( this->suspended()  )
   {
     this->get_target().perform_incoming( std::move( holder ), io_id, std::move(handler) );
-    return;
   }
   else
   {
@@ -76,22 +35,6 @@ void queue::perform_incoming(incoming_holder holder, io_id_t io_id, rpc_outgoing
       t.perform_incoming( std::move( *pholder ), io_id, this->make_handler_( std::move(handler) ) );
     });
   }
-  /*
-  else if ( auto w = _incoming )
-  {
-    auto pholder = std::make_shared<incoming_holder>( std::move(holder) );
-    w->post([pholder, io_id, handler, this]() mutable
-    {
-      auto t = this->target();
-      t.perform_incoming( std::move( *pholder ), io_id, this->make_handler_( std::move(handler) ) );
-    });
-  }
-  else
-  {
-    auto t = this->target();
-    t.perform_incoming( std::move( holder ), io_id, this->make_handler_( std::move(handler) ) );
-  }
-  */
 }
   
 void queue::perform_outgoing(outgoing_holder holder, io_id_t io_id)
@@ -109,21 +52,6 @@ void queue::perform_outgoing(outgoing_holder holder, io_id_t io_id)
       t.perform_outgoing( std::move( *pholder ), io_id );
     });
   }
-    /*if ( auto w = _incoming )
-  {
-    auto pholder = std::make_shared<outgoing_holder>( std::move(holder) );
-    w->post([pholder, io_id, this]()
-    {
-      auto t = this->target();
-      t.perform_outgoing( std::move( *pholder ), io_id );
-    });
-  }
-  else
-  {
-    auto t = this->target();
-    t.perform_outgoing( std::move( holder ), io_id );
-  }
-  */
 }
 
 queue::rpc_outgoing_handler_t queue::make_handler_(rpc_outgoing_handler_t&& handler)
